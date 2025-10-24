bc8333e530c9797f32d602adfdf4a10c1956b1e4
mariannaaspuria <aspuria.mp@gmail.com>
2025-10-24 10:31:59 -0500

    add field members to vehicle class

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,5 +1,17 @@
 package com.pluralsight;
 
 public class Vehicle {
+    // field members
+    private int vin;
+    private int year;
+    private String make;
+    private String model
+    private String vehicleType;
+    private String color;
+    private int odometer;
+    private double price;
+
+    // constructor
+
 
 }
